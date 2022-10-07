276f05b3f11c0c66481fa86fa55b121950449436
jonathan lucas <113566274+jonathanluca@users.noreply.github.com>
2022-10-07 00:53:53 -0300

    criando funções e passando parametros

---
+++ functions.js
@@ -0,0 +1,23 @@
+//criando functions
+
+function darBoasVindas (nome) {
+    console.log(`Olã ${nome}`)
+}
+// passando parametros para função
+// craindo função 
+// interpolação e strings
+
+darBoasVindas("felipe")
+
+
+function maiorIdade (idade) {
+    console.log(`voce tem ${idade} anos`)
+    if( idade >= 18){
+        console.log("esta liberado")
+    }
+    else {
+        console.log("acesse negado")
+
+    }
+}
+maiorIdade(18)
