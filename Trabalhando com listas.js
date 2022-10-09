d7f645b3b698294847f54e16a4dc2d17c8b4c23b
jonathan lucas <113566274+jonathanluca@users.noreply.github.com>
2022-10-09 17:46:04 -0300

    Create Trabalhando com listas.js

---
+++ Trabalhando com listas.js
@@ -0,0 +1,17 @@
+console.log(`trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listasDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listasDeDestinos.push(`Curitiba`) //adicionando
+
+console.log("Destinos possiveis:");
+// console.log(salvador, saoPaulo, rioDeJaneiro);
+console.log(listasDeDestinos);
+
